409d65594ab4c93f13382f9ce7d64a680ba37cf0
kenzasanaa <kessi.k@edu.spbstu.ru>
2026-09-07 11:59:57 +0300

    Add lab 3 variant 33 kessi.kenza

--- kessi.kenza/lab3/solution.js
+++ kessi.kenza/lab3/solution.js
@@ -1,11 +1,10 @@
 export function shuffleArray(arr) {
-    const result = [...arr];   // copy to avoid changing the original
-    let i = result.length;     // start from the end
-
+    const result = [...arr];  
+    let i = result.length;     
     while (i > 0) {
-        const j = Math.floor(Math.random() * i); // random index from 0 to i-1
-        i--;                                     // move backward
-        [result[i], result[j]] = [result[j], result[i]]; // swap
+        const j = Math.floor(Math.random() * i); 
+        i--;                                     
+        [result[i], result[j]] = [result[j], result[i]]; 
     }
 
     return result;
